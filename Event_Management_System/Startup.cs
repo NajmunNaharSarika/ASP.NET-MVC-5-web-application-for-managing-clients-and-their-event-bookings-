7c821e15c9d885a65290cd35ce9754d6aa3b9d0f
Najmun Naher <naz.moon1998@gmail.com>
2026-04-19 04:18:14 -0500

    Add files via upload

---
+++ Event_Management_System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Event_Management_System.Startup))]
+namespace Event_Management_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
